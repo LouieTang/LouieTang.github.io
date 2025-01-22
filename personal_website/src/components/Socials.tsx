6bc6652cc98348936d1a295e851109d900626e5a
LouieTang <louietang2013@gmail.com>
2025-01-21 23:26:08 -0800

    patch

--- personal_website/src/components/Socials.tsx
+++ personal_website/src/components/Socials.tsx
@@ -9,7 +9,7 @@ import '../styles/Socials.css';
 function Socials() {
     return (
         <div className="socials-container">
-            <a href="https://louietang.github.io" target="_blank" className="socials"><img className="icon-l" src={githubIcon} alt="GitHub" /></a>
+            <a href="https://github.com/LouieTang" target="_blank" className="socials"><img className="icon-l" src={githubIcon} alt="GitHub" /></a>
             <a href="https://www.linkedin.com/in/louie-tang" target="_blank" className="socials"><img className="icon-l" src={linkedinIcon} alt="LinkedIn" /></a>
             <a href="mailto:404notfoundlouie@gmail.com" className="socials"><img className="icon-l email" src={emailIcon} alt="Email" /></a>
             <a href="https://leetcode.com/u/LouieTang/" target="_blank" className="socials"><img className="icon-l" src={leetcodeIcon} alt="LeetCode" /></a>
